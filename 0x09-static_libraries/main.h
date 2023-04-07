b83fa83a76afdc7cc4b5b225ae69c21dc9825875
elouardy1 <elouardyabderrahim12@example.com>
2023-04-07 07:36:28 -0700

    0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char a);
-int _islower(int b);
+int _putchar(char c);
+int _islower(int c);
 int _isalpha(int c);
-int _abs(int d);
-int _isupper(int e);
-int _isdigit(int f);
-int _strlen(char *g);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
@@ -20,4 +20,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif /* MAIN_H */
